c7af8ed5abefe5547c5d181de1fec1f50a42edee
InGyu Moon <ansdlsrb1121@naver.com>
2023-11-26 21:12:36 +0900

    first commit

---
+++ src/main/kotlin/com/example/LottoSpring/LottoSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.LottoSpring // ktlint-disable package-name
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LottoSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<LottoSpringApplication>(*args)
+}
